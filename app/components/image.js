0b5dd349aa7d0469206a92689c3aac600639bca5
peterh-earnup <phirsch@earnup.com>
2022-04-30 15:22:12 -0700

    initial commit

---
+++ app/components/image.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../index.css';
+
+export default class Image extends React.Component {
+	componentDidMount() {
+		this.observer = new IntersectionObserver(entries => {
+			// console.log('entries: ', entries)
+			entries.forEach(entry => {
+				const { isIntersecting, intersectionRatio } = entry
+
+				// const wrapperDiv = document.querySelector("#imgWrapper" + this.props.idx)
+				// console.log('wrapperDiv.getBoundingClientRect: ', wrapperDiv.getBoundingClientRect())
+				// console.log('intersectionRatio: ', intersectionRatio)
+
+				if (isIntersecting) {
+					// console.log('isIntersecting, src : ', this.props.src)
+					
+					this.element.src = this.props.src
+					// console.log('this.element.getBoundingClientRect: ', this.element.getBoundingClientRect())
+					// console.log('this.element.getClientRects: ', this.element.getClientRects())
+					this.observer = this.observer.disconnect()
+				}
+			})
+		}, {
+			root: null,
+			threshold: 0.1,
+			// root: document.querySelector("#container")
+			// document.querySelector("#imgWrapper" + this.props.idx)
+		})
+
+		// console.log('this.element: ', this.element)
+		this.observer.observe(document.querySelector("#imgWrapper" + this.props.idx)
+			// this.element
+		)
+	}
+
+	render() {
+		return (
+			<div id={'imgWrapper' + this.props.idx} style={{minHeight: '400px'}}>
+				<img ref={el => this.element = el} style={{maxWidth: '100%', maxHeight:'700px', width: 'auto', height: 'auto'}}/>
+			</div>
+		)
+	}
+}
---
+++ app/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Image from './components/image'
+import { content } from './content'
+
+				// <Image
+				// 	src=''
+				// 	name=''
+				// 	tags={['']}
+                //  idx={0}
+				// />
+
+class App extends React.Component {
+    render() { 
+        return (
+            <div id='container'>
+                {content.map(imageData => <Image
+                    src={imageData.src}
+                    name={imageData.name}
+                    tags={imageData.tags}
+                    idx={imageData.idx}
+                />)}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
